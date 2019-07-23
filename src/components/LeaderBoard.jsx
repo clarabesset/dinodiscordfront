2509e4e62e72b1f0b54c5ed446eedc766c930e8e
clarabesset <clara.besset@gmail.com>
2019-07-23 15:58:23 +0200

    leaderboard changes

--- src/components/LeaderBoard.jsx
+++ src/components/LeaderBoard.jsx
@@ -1,54 +1,41 @@
-import React, { Component } from 'react';
-import DinoRed from './../img/red_dino_short.gif';
-
+import React, { Component } from "react";
+import DinoRed from "./../img/red_dino_short.gif";
+import axios from "axios";
 export default class LeaderBoard extends Component {
-	render() {
-		return (
-			<div className="leaderboard">
-				<h1>Leaderboard</h1>
-				<table className="table">
-					<thead>
-						<tr>
-							<th><img src={DinoRed} alt="dino"/></th>
-							<th>Player</th>
-							<th>Points</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr>
-							<td>1</td>
-							<td>Dino </td>
-							<td>6789 points</td>
-						</tr>
-                        <tr>
-							<td>1</td>
-							<td>Dino </td>
-							<td>6789 points</td>
-						</tr>
-                        <tr>
-							<td>1</td>
-							<td>Dino </td>
-							<td>6789 points</td>
-						</tr>
-                        <tr>
-							<td>1</td>
-							<td>Dino </td>
-							<td>6789 points</td>
-						</tr>
-                        <tr>
-							<td>1</td>
-							<td>Dino </td>
-							<td>6789 points</td>
-						</tr>
-                        <tr>
-							<td>1</td>
-							<td>Dino </td>
-							<td>6789 points</td>
-						</tr>
-                        
-					</tbody>
-				</table>
-			</div>
-		);
-	}
+  state = {
+    users: []
+  };
+  componentDidMount() {
+    axios.get("http://localhost:3001/api/User").then(res => {
+      console.log(res);
+      this.setState({ users: res.data });
+    });
+  }
+  render() {
+    return (
+      <div className="leaderboard">
+        <h1>Leaderboard</h1>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>
+                <img src={DinoRed} alt="dino" />
+              </th>
+              <th>Player</th>
+              <th>Points</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.users.map(users => (
+              <tr>
+                <td>1</td>
+                <td>{users.username} </td>
+                <td>{users.score}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
