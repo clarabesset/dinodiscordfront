32e5ce139defceca88d4211a1200564e0da48a36
clarabesset <clara.besset@gmail.com>
2019-07-24 10:50:29 +0200

    css pages looks cuter

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
   return (
     <div>
-      <footer className='footer'>
-        <p className='footer-title'>Made with humour & passion by Camille, Clara, Grégoire & Octave - Enjoy your game ;)</p>
+      <footer className="footer">
+        <p className="footer-title">
+          Made with humour & passion by Camille, Clara, Grégoire & Octave -
+          Enjoy the game :)
+        </p>
       </footer>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/NavMain.jsx
+++ src/components/NavMain.jsx
@@ -1,33 +1,39 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { AuthConsumer } from './../auth/Guard'
+import { AuthConsumer } from "./../auth/Guard";
+
 function NavMain() {
   return (
     <AuthConsumer>
       {({ loginStatus, signout }) => {
-        console.log("waazzaaaa", loginStatus)
-        return (<>
-          <div>
-            <nav className='navMain'>
-              <div>
-                <NavLink to='/menu' className='navMain-title'>DINO DISCORD</NavLink>
-              </div>
-              <div className='navMain-choices'>
-                <NavLink to='/menu' className='nav-item'>profil</NavLink>
-                <NavLink to='/signin' className='nav-item'>login</NavLink>
-                <button onClick={() => signout(res => console.log(res))} className='nav-item'>logout</button>
-              </div>
-            </nav>
-          </div>
-        </>)
-      }
-      }
+        console.log("waazzaaaa", loginStatus);
+        return (
+          <>
+            <div>
+              <nav className="navMain">
+                <div>
+                  <NavLink to="/menu" className="navMain-title">
+                    DINO DISCORD
+                  </NavLink>
+                </div>
+                <div className="navMain-choices">
+                  <NavLink to="/profile" className="nav-item">
+                    profile
+                  </NavLink>
+                  <NavLink
+                    onClick={() => signout(res => console.log(res))}
+                    className="nav-item"
+                  >
+                    log out
+                  </NavLink>
+                </div>
+              </nav>
+            </div>
+          </>
+        );
+      }}
     </AuthConsumer>
-  )
-
+  );
 }
-
-
-
 
 export default NavMain;
