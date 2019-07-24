6b21456fd0458527327f01abea39be4782eed352
Grégoire Lescuyer <gregoirelescuyer@gmail.com>
2019-07-24 09:24:08 +0200

    infoplayer.jsx

---
+++ src/components/InfoPlayer.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class InfoPlayer extends Component {
+    render() {
+        return (
+            <div>
+                <p>heyyy</p>
+            </div>
+        )
+    }
+}
