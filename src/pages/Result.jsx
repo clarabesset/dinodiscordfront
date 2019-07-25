e14d13be6e85dd3ff326c0ddf7f7b0a69a41896f
clarabesset <clara.besset@gmail.com>
2019-07-25 19:31:58 +0200

    crown and result screen kinda done

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,29 +1,44 @@
 import React, { Component } from "react";
 import MusicWin from "../components/MusicWin";
-import blue from './../img/blue_dino_short.gif';
-import green from './../img/green_dino_short.gif';
-import yellow from './../img/yellow_dino_short.gif';
-import red from './../img/red_dino_short.gif';
+import blue from "./../img/blue_dino_short.gif";
+import green from "./../img/green_dino_short.gif";
+import yellow from "./../img/yellow_dino_short.gif";
+import red from "./../img/red_dino_short.gif";
 
 const colors = {
-  blue, red, yellow, green
-}
+  blue,
+  red,
+  yellow,
+  green
+};
 
 export default class Result extends Component {
   render() {
     return (
       <div className="resultContainer">
         <MusicWin />
-        {this.props.result && <React.Fragment>
-        <div className="titleResult">* Player {this.props.result.winner} you win!!</div>
-        <div className="containerWinDino">
-          <div class="box1 sb7">with {this.props.result[this.props.result.winner]} points bitch</div>
-          <div className="gifWinner">
-              <img src={colors[this.props.result.winnerColor]} alt="la win frère"/>
+        {this.props.result && (
+          <React.Fragment>
+            <div className="titleResult">
+              Player {this.props.result.winner} you win!!
             </div>
-        </div>
-        </React.Fragment>
-        }
+            <div className="containerWinDino">
+              <div className="crowntext">
+                <div className="crown" />
+                <div class="box1 sb7">
+                  with {this.props.result[this.props.result.winner]} points
+                  bitch
+                </div>
+              </div>
+              <div className="gifWinner">
+                <img
+                  src={colors[this.props.result.winnerColor]}
+                  alt="la win frère"
+                />
+              </div>
+            </div>
+          </React.Fragment>
+        )}
       </div>
     );
   }
