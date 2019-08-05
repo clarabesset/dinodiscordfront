936798bb5b0db1258b9962b32184ca0dd94da430
Camille Charpentier <cam.charpentier@live.fr>
2019-08-05 10:46:48 +0200

    cleaned Player.jsx

--- src/pages/Player.jsx
+++ src/pages/Player.jsx
@@ -2,9 +2,9 @@ import React, { Component } from "react";
 
 export default class Player extends Component {
   state = {
-    numero: 1,
-    color: null,
-    score: 0,
+    numero: 1, // numéro de joueur pendant la partie
+    color: null, // couleur du joueur
+    score: 0, // score de la partie
     x: null,
     y: null
   };
