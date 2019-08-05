f6b77e35236d7841abf4e700d4e5b073daba1d22
Camille Charpentier <cam.charpentier@live.fr>
2019-08-05 10:46:31 +0200

    cleaned Player.jsx

--- src/pages/Player.jsx
+++ src/pages/Player.jsx
@@ -5,14 +5,9 @@ export default class Player extends Component {
     numero: 1,
     color: null,
     score: 0,
-    direction: "N",
     x: null,
     y: null
   };
-
-  // function generateBoard() {
-
-  // }
 
   render() {
     return <div className="player" />;
